cb7ed3a23b4b98cc4b58ec42a4f8619110cbf9b0
Luís Carlos Ribeiro Junior - LRB <luis.ribeiro@iteris.com.br>
2024-06-05 19:25:01 -0300

    feat: add logger and debouncer

--- lib/app/(modules)/devices/interactor/controllers/home_page_controller.dart
+++ lib/app/(modules)/devices/interactor/controllers/home_page_controller.dart
@@ -1,14 +1,17 @@
 import 'dart:io';
 
-import 'package:multiroom/app/core/extensions/list_extensions.dart';
-import 'package:multiroom/app/core/models/equalizer_model.dart';
-import 'package:multiroom/app/core/models/frequency.dart';
-import 'package:multiroom/app/core/models/zone_model.dart';
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:signals/signals_flutter.dart';
 
 import '../../../../core/enums/page_state.dart';
+import '../../../../core/extensions/list_extensions.dart';
 import '../../../../core/interactor/controllers/base_controller.dart';
+import '../../../../core/models/equalizer_model.dart';
+import '../../../../core/models/frequency.dart';
 import '../../../../core/models/input_model.dart';
+import '../../../../core/models/zone_model.dart';
+import '../../../../core/utils/debouncer.dart';
 import '../models/device_model.dart';
 
 class HomePageController extends BaseController {
@@ -58,12 +61,16 @@ class HomePageController extends BaseController {
     EqualizerModel.builder(name: "Flat", value: 50),
   ]);
 
+  final _logger = Logger();
+
   final host = "192.168.0.12".toSignal(debugLabel: "host");
   final port = "4998".toSignal(debugLabel: "port");
   final isConnected = false.toSignal(debugLabel: "isConnected");
   final device = DeviceModel.empty().toSignal(debugLabel: "device");
   final currentZone = ZoneModel.empty().toSignal(debugLabel: "currentZone");
   final currentInput = InputModel.empty().toSignal(debugLabel: "currentInput");
+
+  final debouncer = Debouncer(delay: Durations.short4);
 
   Future<void> toggleConnection() async {
     if (isConnected.value) {
@@ -108,6 +115,12 @@ class HomePageController extends BaseController {
 
   void setVolume(double volume) {
     currentZone.value = currentZone.value.copyWith(volume: volume.toInt());
+
+    debouncer(() {
+      _socket.writeln("$volume");
+
+      _logger.d("Enviado >>> $volume");
+    });
   }
 
   void setEqualizer(int equalizerIndex) {
