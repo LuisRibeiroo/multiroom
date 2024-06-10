18dc428ae3b68dee9296424ed403fcaeb1ba63d5
Luís Carlos Ribeiro Junior - LRB <luis.ribeiro@iteris.com.br>
2024-06-10 12:06:07 -0300

    build: update build script

--- release.sh
+++ release.sh
@@ -1,17 +1,22 @@
+#! /bin/bash
+
+echo "\n\n--> Flutter clean"
 flutter clean
+
+echo "\n\n--> Flutter pub get"
 flutter pub get
 
-echo "--> Building Android APK"
+echo "\n\n--> Building Android APK"
 flutter build apk --release
 
 mv build/app/outputs/flutter-apk/app-release.apk bin/multiroom.apk
-echo "--> Android build available at: bin/multiroom.apk"
+echo "\n\n--> Android build available at: bin/multiroom.apk"
 
 if [[ "$OSTYPE" == "msys" ]]; then
-  echo "--> Building Windows EXE"
+  echo "\n\n--> Building Windows EXE"
   flutter build windows --release
 
   mv -r build/windows/runner/Release bin/windows
   powershell Compress-Archive bin\windows bin\windows.zip
-  echo "--> Windows build available at: bin/windows"
+  echo "\n\n--> Windows build available at: bin/windows"
 fi
