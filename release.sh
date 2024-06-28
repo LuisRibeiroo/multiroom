3be720b34b3f73ba68c232c9477bd00e8632823c
LuisRibeiroo <dev.luisribeiro@outlook.com>
2024-06-28 00:13:11 -0300

    chore: update build script

--- release.sh
+++ release.sh
@@ -9,5 +9,12 @@ flutter pub get
 printf "\n\n--> Building Android APK"
 flutter build apk --release
 
+
+printf "\n\n--> Building MAC App"
+flutter build macos --release
+
 mv build/app/outputs/flutter-apk/app-release.apk bin/multiroom.apk
-printf "\n\n--> Android build available at: bin/multiroom.apk\n"
+mv build/macos/Build/Products/Release/multiroom.app bin/multiroom.app
+
+printf "\n\n--> Android build available at: bin/multiroom.apk"
+printf "\n--> MacOS build available at: bin/multiroom.app\n"
