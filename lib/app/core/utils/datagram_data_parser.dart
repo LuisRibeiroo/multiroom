09f780fc79b9e41a16d59995d7fca083482afe15
Luís Ribeiro <dev.luisribeiro@outlook.com>
2024-07-24 20:04:30 -0300

    refactor: change firmware decimals print

--- lib/app/core/utils/datagram_data_parser.dart
+++ lib/app/core/utils/datagram_data_parser.dart
@@ -3,10 +3,10 @@ import 'dart:typed_data';
 abstract class DatagramDataParser {
   static (String serialNumber, String firmware) getSerialAndFirmware(Uint8List data) {
     String serialNumber = String.fromCharCodes(data.sublist(29, 45));
-    String firmware = ((data.sublist(45, 46).toList().first + (data.sublist(46, 47).toList().first * 256)) / 1000)
-        .toDouble()
-        .toStringAsFixed(1);
+    final firmware =
+        ((data.sublist(45, 46).toList().first + (data.sublist(46, 47).toList().first * 256)) / 1000).toString();
+    final formatted = "${firmware.substring(0, 2)}${firmware.substring(3)}";
 
-    return (serialNumber, firmware);
+    return (serialNumber, formatted);
   }
 }
