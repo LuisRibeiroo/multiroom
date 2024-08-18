f5eb0f3317779b67879350c3671cb8a973c3e02d
Luís Ribeiro <dev.luisribeiro@outlook.com>
2024-08-18 13:38:25 -0300

    fix: firmware version check

--- lib/app/core/utils/datagram_data_parser.dart
+++ lib/app/core/utils/datagram_data_parser.dart
@@ -1,11 +1,14 @@
 import 'dart:typed_data';
 
+import 'package:multiroom/app/core/extensions/string_extensions.dart';
+
 abstract class DatagramDataParser {
   static (String serialNumber, String firmware) getSerialAndFirmware(Uint8List data) {
     String serialNumber = String.fromCharCodes(data.sublist(29, 45));
     final firmware =
         ((data.sublist(45, 46).toList().first + (data.sublist(46, 47).toList().first * 256)) / 1000).toString();
-    final formatted = "${firmware.substring(0, 2)}${firmware.substring(3)}";
+    final formatted =
+        "${firmware.substring(0, 2)}${firmware.substring(3).isNotNullOrEmpty ? firmware.substring(3) : firmware.substring(2)}";
 
     return (serialNumber, formatted);
   }
