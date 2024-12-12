6f9122eb26932b4ad847d2246a2c7ccd58870887
Luís Ribeiro <dev.luisribeiro@outlook.com>
2024-12-12 20:44:51 -0300

    fix: update channel names

--- lib/app/modules/home/pages/home_page.dart
+++ lib/app/modules/home/pages/home_page.dart
@@ -88,16 +88,17 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
   }
 
   void _showChannelsBottomSheet({ZoneModel? zone}) {
-    context.showCustomModalBottomSheet(
-      // isScrollControlled: false,
-      child: Watch(
-        (_) => EditChannelsBottomSheet(
-          onSelect: _controller.setCurrentChannel,
-          device: _controller.currentDevice.value,
-          zone: zone ?? _controller.currentZone.value,
-        ),
-      ),
-    );
+    context
+        .showCustomModalBottomSheet(
+          child: Watch(
+            (_) => EditChannelsBottomSheet(
+              onSelect: _controller.setCurrentChannel,
+              device: _controller.currentDevice.value,
+              zone: zone ?? _controller.currentZone.value,
+            ),
+          ),
+        )
+        .then((_) => _controller.syncLocalData(awaitUpdate: false));
   }
 
   void _showEqualizersBottomSheet() {
